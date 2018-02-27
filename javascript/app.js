c2a304e115118e3ffd8db83e558ed8cdc2b373c5
Falon Darville <falondarville@gmail.com>
2018-02-26 21:16:54 -0800

    add variables and started functions

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,51 @@
 $(document).ready(function() {
 
+//API information
+var apiKey = "bd2ce8623fb048b697f2ddbc2abef055";
+var baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey;
 
-console.log("this is linked")
+//Search parameters inputted on form
+var queryTerm = "";
+var numberResults = 0;
+var startYear = 0;
+var endYear = 0;
+
+//number of articles initializing counter
+var articleCounter = 0;
+
+//onclick function to retrieve articles
+function runQuery(numberArticles, queryURL) {
+
+	$.ajax({
+		crossOrigin: true,
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+
+		console.log(response);
+	});
+
+
+};
+
+
+$("#searchButton").on("click", function(){
+
+	queryTerm = $("#search").val().trim();
+
+	var newURL = baseURL + "&q=" + queryTerm;
+
+	console.log(newURL);
+
+	runQuery();
+	return false;
+
+});
+
+
+
+
+
 
 
 
