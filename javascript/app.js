120c87bcae5a21e0fb54e9cc6b3927bc8dbd00ca
Falon Darville <falondarville@gmail.com>
2018-02-27 10:36:02 -0800

    add variables for query URL constructor

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+//use localhost through MAMP to test code, issue otherwise with cross origin
 
 //API information
 var apiKey = "bd2ce8623fb048b697f2ddbc2abef055";
@@ -24,26 +25,22 @@ function runQuery(numberArticles, queryURL) {
 
 		console.log(response);
 	});
-
-
 };
 
 
-$("#searchButton").on("click", function(){
+$("#searchButton").on("click", function(event){
 
+	event.preventDefault();
 	queryTerm = $("#search").val().trim();
+	startYear = $("start").val().trim() + 0101
+	endYear = $("end").val().trim() + 1231
+	numberResults = $("#numberRecords").val().trim();
 
-	var newURL = baseURL + "&q=" + queryTerm;
+	var newURL = baseURL + "&q=" + queryTerm + ";
 
-	console.log(newURL);
-
-	runQuery();
-	return false;
+	runQuery(numberResults, newURL);
 
 });
-
-
-
 
 
 
